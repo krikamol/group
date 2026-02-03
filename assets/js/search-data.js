3cc7b243d0d1ab9e8cb917cc5d0a5e465cbef312
Anurag14 <tellanurag95@gmail.com>
2026-02-03 14:14:40 +0000

    Deploying to gh-pages from @ krikamol/group@a2107bf417da0c7ae63817063c23034647287904 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,11 +55,11 @@ ninja.data = [{
           window.open("https://medium.com/@anurags.it/imprecise-learning-for-dummies-a-new-perspective-on-training-ml-models-for-real-world-0021d2ba5eb7?source=rss-4e6f7d0c8689------2", "_blank");
         
       },
-    },{id: "post-imprecise-learning-for-dummies-how-to-solve-for-human-ai-alignment-medium",
+    },{id: "post-just-a-moment",
       
-        title: 'Imprecise Learning for dummies: How to solve for Human AI-Alignment | Medium <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: 'Just a moment... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
-      description: "Imprecise Learning for dummies: A new perspective on training ML models for real-world In the real world, new conditions and changing scenarios often differ from training data, causing current ML …",
+      description: "",
       section: "Posts",
       handler: () => {
         
