7e3ae03caf4ff386c81b78aa643f97a4ad8d7057
Anurag14 <tellanurag95@gmail.com>
2026-03-17 22:53:05 +0000

    Deploying to gh-pages from @ krikamol/group@4a169f482ccce324d6194eb23e9641fbd1136b9d 🚀

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,12 @@
 // Initialize medium zoom.
 $(document).ready(function () {
+  let zoomBg = getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color").trim();
+  
+  if (zoomBg.length === 4 && zoomBg.startsWith('#')) {
+    zoomBg = '#' + zoomBg[1] + zoomBg[1] + zoomBg[2] + zoomBg[2] + zoomBg[3] + zoomBg[3];
+  }
+
   medium_zoom = mediumZoom("[data-zoomable]", {
-    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+    background: zoomBg + "ee", // + 'ee' for trasparency.
   });
 });
