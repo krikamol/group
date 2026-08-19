d09980e2bb8fccdb4df6052a489add53f42fa6ca
krikamol <krikamol@gmail.com>
2026-08-19 14:24:10 +0000

    Deploying to gh-pages from @ krikamol/group@5138782e9f0c1da09fcb30d3b3d2cb44781338c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,9 +55,9 @@ ninja.data = [{
           window.open("https://medium.com/@anurags.it/imprecise-learning-for-dummies-a-new-perspective-on-training-ml-models-for-real-world-0021d2ba5eb7?source=rss-4e6f7d0c8689------2", "_blank");
         
       },
-    },{id: "post-just-a-moment",
+    },{id: "post-attention-required-cloudflare",
       
-        title: 'Just a moment... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: 'Attention Required! | Cloudflare <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
       description: "",
       section: "Posts",
